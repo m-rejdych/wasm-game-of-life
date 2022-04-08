02ce7a0b290e9d256275bca56e39850cfaf59185
m-rejdych <rejdych.m.r@gmail.com>
2022-04-09 01:18:11 +0200

    Removed nested repository.

---
+++ www/index.js
@@ -0,0 +1,73 @@
+import { Universe, Cell } from 'wasm-game-of-life';
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
+
+const CELL_SIZE = 10;
+const GRID_COLOR = '#CCCCCC';
+const ALIVE_COLOR = '#000000';
+const DEAD_COLOR = '#FFFFFF';
+
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById('game-of-life-canvas');
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let i = 0; i <= height; i++) {
+    ctx.moveTo(0, i * (CELL_SIZE, +1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, i * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => row * width + column;
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+const renderLoop = () => {
+  universe.tick();
+
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+};
+
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLoop);
